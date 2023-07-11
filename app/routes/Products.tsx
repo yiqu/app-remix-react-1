1aa540e39ab57cdf72dfd77d6a604c2ee085bf34
Kevin Qu <kevinqu88@gmail.com>
2023-07-10 23:58:58 -0400

    try

--- app/routes/Products.tsx
+++ app/routes/Products.tsx
@@ -39,34 +39,34 @@ function Products() {
 export default Products;
 
 // this is not client code, only server usage. 
-export async function action({ request, context, params }: ActionArgs) {
-  const body = await request.formData();
-  console.log("METHOD: ", request.method, Object.fromEntries(body));
-  const create$ = await fetchGet<FirebaseResponse>(`https://kq-1-1a499.firebaseio.com/remix-1-products.json`, "POST", Object.fromEntries(body));
-  return json(create$);
-}
+// export async function action({ request, context, params }: ActionArgs) {
+//   const body = await request.formData();
+//   console.log("METHOD: ", request.method, Object.fromEntries(body));
+//   const create$ = await fetchGet<FirebaseResponse>(`https://kq-1-1a499.firebaseio.com/remix-1-products.json`, "POST", Object.fromEntries(body));
+//   return json(create$);
+// }
 
-export async function loader({ request, params }: LoaderArgs) {
-  let { searchParams } = new URL(request.url);
-  let someSearchParam = Number(searchParams.get("myCoolParams") ?? 0);
-  let teamId = 1;
+// export async function loader({ request, params }: LoaderArgs) {
+//   let { searchParams } = new URL(request.url);
+//   let someSearchParam = Number(searchParams.get("myCoolParams") ?? 0);
+//   let teamId = 1;
 
-  // validate teamId exists, because search params can be string or undefined
-  if (!teamId) return json({ error: "missing teamId" }, 400);
+//   // validate teamId exists, because search params can be string or undefined
+//   if (!teamId) return json({ error: "missing teamId" }, 400);
 
-  const result = await fetchGet<FirebaseResult<Product>>(`https://kq-1-1a499.firebaseio.com/remix-1-products.json`, "GET");
+//   const result = await fetchGet<FirebaseResult<Product>>(`https://kq-1-1a499.firebaseio.com/remix-1-products.json`, "GET");
 
-  const keys = Object.keys(result);
-  const products: ProductFire[] = [];
-  keys.forEach((key) => {
-    products.push({
-      fireId: key,
-      ...result[key]
-    });
-  });
-  products.reverse();
-  return json(products);
-}
+//   const keys = Object.keys(result);
+//   const products: ProductFire[] = [];
+//   keys.forEach((key) => {
+//     products.push({
+//       fireId: key,
+//       ...result[key]
+//     });
+//   });
+//   products.reverse();
+//   return json(products);
+// }
 
 export function ErrorBoundary() {
   return (
